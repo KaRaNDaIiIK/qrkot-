932762310c775c20f5fbd66dcaedfa3b5c22315b
KaRaNDaIiIK <donchev.yus@yandex.ru>
2026-05-21 14:14:22 +0500

    Реадми

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -9,22 +9,34 @@ router = APIRouter()
 router.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix='/auth/jwt',
-    tags=['auth'],
+    tags=['Аутентификация'],
 )
 
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix='/auth',
-    tags=['auth'],
+    tags=['Аутентификация'],
 )
 
 users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
 users_router.routes = [
     route for route in users_router.routes
-    if not (isinstance(route, APIRoute) and route.name == 'users:delete_user')  # TODO: Не понятная проблема!
+    if not (isinstance(route, APIRoute) and route.name == 'users:delete_user')
 ]
+
+for route in users_router.routes:
+    if isinstance(route, APIRoute):
+        if route.name == 'users:current_user':
+            route.summary = 'Текущий пользователь'
+        elif route.name == 'users:patch_current_user':
+            route.summary = 'Обновить текущего пользователя'
+        elif route.name == 'users:user':
+            route.summary = 'Получить пользователя по ID'
+        elif route.name == 'users:patch_user':
+            route.summary = 'Обновить пользователя по ID'
+
 router.include_router(
     users_router,
     prefix='/users',
-    tags=['users'],
+    tags=['Пользователи'],
 )
